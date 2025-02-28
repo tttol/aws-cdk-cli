8a73428b8f8c3d01e78bfe849d5dc0ad50a2e4ff
Rico Hermans <rix0rrr@gmail.com>
2025-02-28 12:16:32 +0100

    chore: update to latest version of integ workflow (#169)
    This one should also work from forks.
    
    ---
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license

--- .projenrc.ts
+++ .projenrc.ts
@@ -164,7 +164,7 @@ const repoProject = new yarn.Monorepo({
 
   defaultReleaseBranch: 'main',
   devDeps: [
-    'cdklabs-projen-project-types@^0.1.220',
+    'cdklabs-projen-project-types',
     'glob',
     'semver',
     `@aws-sdk/client-s3@${CLI_SDK_V3_RANGE}`,
@@ -1328,11 +1328,11 @@ const APPROVAL_ENVIRONMENT = 'integ-approval';
 const TEST_ENVIRONMENT = 'run-tests';
 
 new CdkCliIntegTestsWorkflow(repo, {
+  sourceRepo: 'aws/aws-cdk-cli',
   approvalEnvironment: APPROVAL_ENVIRONMENT,
   testEnvironment: TEST_ENVIRONMENT,
   buildRunsOn: POWERFUL_RUNNER,
   testRunsOn: POWERFUL_RUNNER,
-  expectNewCliLibVersion: true,
 
   localPackages: [
     // CloudAssemblySchema is not in this list because in the way we're doing
