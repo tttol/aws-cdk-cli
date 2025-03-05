c46717c9c606e06598a112f7d7e92e61e237c1aa
Rico Hermans <rix0rrr@gmail.com>
2025-03-05 20:11:20 +0100

    chore: publish `cdk-assets` as `latest` (#189)
    We are currently running `cdk-assets` v3 as `latest`, and it is working
    fine. Make this the official disttag for this version.
    
    ---
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license

--- .projenrc.ts
+++ .projenrc.ts
@@ -639,7 +639,6 @@ const cdkAssets = configureProject(
         run: 'npx projen shrinkwrap',
       },
     ],
-    npmDistTag: 'v3-latest',
     majorVersion: 3,
 
     jestOptions: jestOptionsForProject({
