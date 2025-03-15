b73dc5b46bb39bc60a882ed15b82aa68311b2386
t-takahashi <tttol.t4k.b1z@gmail.com>
2025-03-15 15:30:41 +0900

    update legacy-exports

--- packages/aws-cdk/lib/legacy-exports-source.ts
+++ packages/aws-cdk/lib/legacy-exports-source.ts
@@ -4,30 +4,27 @@
 // See https://github.com/aws/aws-cdk/pull/33021 for more information.
 
 // Note: All type exports are in `legacy-exports.ts`
+export * from './legacy-logging-source';
+export { deepClone, flatten, ifDefined, isArray, isEmpty, numberFromBool, partition, padLeft as leftPad, contentHash, deepMerge } from './util';
+export { deployStack } from './api/deployments/deploy-stack';
+export { cli, exec } from './cli/cli';
 export { SdkProvider } from './api/aws-auth';
-export { AwsCliCompatible } from './api/aws-auth/awscli-compatible';
-export { cached } from './api/aws-auth/cached';
-export { CredentialPlugins } from './api/aws-auth/credential-plugins';
-export { setSdkTracing as enableTracing } from './api/aws-auth/tracing';
+export { PluginHost } from './api/plugin';
+export { Command, Configuration, PROJECT_CONTEXT } from './cli/user-configuration';
+export { Settings } from './api/settings';
 export { Bootstrapper } from './api/bootstrap';
 export { CloudExecutable } from './api/cxapp/cloud-executable';
 export { execProgram } from './api/cxapp/exec';
-export { Deployments } from './api/deployments';
-export { deployStack } from './api/deployments/deploy-stack';
-export { CfnEvaluationException } from './api/evaluate-cloudformation-template';
-export { lowerCaseFirstCharacter } from './api/hotswap/common';
-export { PluginHost } from './api/plugin';
-export { Settings } from './api/settings';
-export { cli, exec } from './cli/cli';
-export { Command, Configuration, PROJECT_CONTEXT } from './cli/user-configuration';
-export { formatAsBanner } from './cli/util/console-formatters';
-export { getVersionMessages as latestVersionIfHigher, versionNumber } from './cli/version';
-export { aliases, command, describe } from './commands/docs';
 export { RequireApproval } from './diff';
-export { availableInitTemplates } from './init';
-export * from './legacy-logging-source';
-export { deepClone, flatten, ifDefined, isArray, isEmpty, numberFromBool, partition } from './util';
-export { contentHash } from './util/content-hash';
+export { formatAsBanner } from './cli/util/console-formatters';
+export { setSdkTracing as enableTracing } from './api/aws-auth/tracing';
+export { aliases, command, describe } from './commands/docs';
+export { lowerCaseFirstCharacter } from './api/hotswap/common';
+export { Deployments } from './api/deployments';
 export { cliRootDir as rootDir } from './cli/root-dir';
-export { deepMerge } from './util/objects';
-export { leftPad } from './util/string-manipulation';
+export { getVersionMessages, versionNumber } from './cli/version';
+export { availableInitTemplates } from './init';
+export { cached } from './api/aws-auth/cached';
+export { CfnEvaluationException } from './api/evaluate-cloudformation-template';
+export { CredentialPlugins } from './api/aws-auth/credential-plugins';
+export { AwsCliCompatible } from './api/aws-auth/awscli-compatible';
--- packages/aws-cdk/lib/legacy-exports.ts
+++ packages/aws-cdk/lib/legacy-exports.ts
@@ -65,7 +65,7 @@ export const {
   deepMerge,
   Deployments,
   rootDir,
-  latestVersionIfHigher,
+  getVersionMessages,
   versionNumber,
   availableInitTemplates,
   cached,
